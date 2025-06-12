cb2a04dc7ae7034b896f87789ea117facd936e32
fangyu-xu <fangyu.xu@trawa.de>
2025-06-12 13:58:31 +0200

    add Sentry init file

---
+++ apps/frontend/src/instrument.ts
@@ -0,0 +1,32 @@
+import * as Sentry from "@sentry/react";
+import { useEffect } from "react";
+
+import {
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router-dom";
+
+Sentry.init({
+  dsn: "<Your Sentry DSN>",
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  _experiments: {
+    enableLogs: true,
+  },
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost:3001"],
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+});
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import "./instrument.js";
+import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-);
+createRoot(document.getElementById("root")!, {
+  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+    console.warn("Uncaught error", error, errorInfo.componentStack);
+  }),
+  onCaughtError: Sentry.reactErrorHandler(),
+  onRecoverableError: Sentry.reactErrorHandler(),
+}).render(<App />);
